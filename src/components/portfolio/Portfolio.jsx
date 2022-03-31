d019bf78adf2ad1f6467b7508e32ab55c6f33325
Sean <seanminches@gmail.com>
2022-03-31 01:30:31 -0400

    pic wont go in for some reason

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,16 @@ import "./portfolio.scss";
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-      portfolio
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">E-Commerce Back End</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="" alt="../../public/assets/textedit.png" />
+          <h3>E</h3>
+        </div>
+      </div>
     </div>
   );
 }
