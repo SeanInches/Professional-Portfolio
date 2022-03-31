accc2ed59c339ba14233277d3684cd74c786440e
Sean <seanminches@gmail.com>
2022-03-31 02:19:32 -0400

    almost done with portfolio. frustrated lol. moving to contact page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,16 +1,56 @@
+import { useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss";
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState("e-commercebackend");
+  const list = [
+    {
+      id: "e-commercebackend",
+      title: "E-Commerce Back End",
+    },
+    {
+      id: "texteditor",
+      title: "Text Editor",
+    },
+    {
+      id: "placeholder",
+      title: "Placeholder",
+    },
+    {
+      id: "placeholder",
+      title: "Placeholder",
+    },
+  ];
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
       <ul>
-        <li className="active">E-Commerce Back End</li>
+        {list.map((item) => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
       </ul>
       <div className="container">
         <div className="item">
-          <img src="" alt="../../public/assets/textedit.png" />
-          <h3>E</h3>
+          <img src="assets/icons8-code-folder-64.png" alt="why no work" />
+          <h3>E-Commerce Back End</h3>
+        </div>
+        <div className="item">
+          <img src="assets/icons8-code-folder-64.png" alt="why no work" />
+          <h3>Text Editor</h3>
+        </div>
+        <div className="item">
+          <img src="assets/icons8-code-folder-64.png" alt="why no work" />
+          <h3>placeholder</h3>
+        </div>
+        <div className="item">
+          <img src="assets/icons8-code-folder-64.png" alt="why no work" />
+          <h3>placeholder</h3>
         </div>
       </div>
     </div>
