df850ab78f2a5e933439420ee31cf85c8e8b1cdb
Sean <seanminches@gmail.com>
2022-03-31 00:34:54 -0400

    front page looking decent

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -7,7 +7,7 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
-            genius.
+            Sean Inches
           </a>
           <div className="itemContainer">
             <Person className="icon" />
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,21 @@ import "./intro.scss";
 export default function Intro() {
   return (
     <div className="intro" id="intro">
-      home
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/20210408_185036.jpg" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Full Stack Developer</h2>
+          <h1>Sean Inches</h1>
+          <h3>Portfolio</h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/outline_expand_more_black_24dp.png" alt="" />
+        </a>
+      </div>
     </div>
   );
 }
